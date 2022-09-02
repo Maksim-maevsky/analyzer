d1da5d4a4ef9b4805c07566a4d70f70cae27bdc3
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2022-09-02 18:25:22 +0400

    refactored cod by sonarlint

--- src/main/java/com/truckplast/analyzer/dto/FileInfoDto.java
+++ src/main/java/com/truckplast/analyzer/dto/FileInfoDto.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class FileInfoDto {
+public class FileInfoDto  implements Serializable {
 
     private String fileName;
 
